ffe3a0a5b27cb534005149202c7082753edcf1e6
Diego Vera <ingvera31@gmail.com>
2015-05-21 01:10:04 -0400

    implement navigationDrawer with recyclerView

--- app/src/main/java/com/veradiego31/sbs/MainActivity.java
+++ app/src/main/java/com/veradiego31/sbs/MainActivity.java
@@ -1,16 +1,19 @@
 package com.veradiego31.sbs;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.app.Fragment;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +21,11 @@ public class MainActivity extends ActionBarActivity {
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
 
-            setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationDrawerFragment navigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
 
+        navigationDrawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
     }
 
     @Override
@@ -40,7 +46,7 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-        if(id == R.id.action_next){
+        if (id == R.id.action_next) {
             startActivity(new Intent(this, SubActivity.class));
         }
 
---
+++ app/src/main/java/com/veradiego31/sbs/NavigationDrawerFragment.java
@@ -0,0 +1,144 @@
+package com.veradiego31.sbs;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NavigationDrawerFragment extends Fragment {
+
+    public  static  final String PREF_FILE_NAME = "testpref";
+
+    public static final String KEY_USER_LEARNED_DRAWER = "user_learned_drawer";
+    private boolean mUserLearnedDrawer;
+    private boolean mFromSavedInstanceState;
+
+    private ActionBarDrawerToggle mDrawerToogle;
+
+    private DrawerLayout mDrawerLayout;
+    private View containerView;
+    private RecyclerView recyclerView;
+    private NavDrawerRecycler adapter;
+
+    public NavigationDrawerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUserLearnedDrawer = Boolean.parseBoolean(readToPreference(getActivity(), KEY_USER_LEARNED_DRAWER, "false"));
+
+        if(savedInstanceState!= null){
+            mFromSavedInstanceState = true;
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View layout = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        recyclerView = (RecyclerView) layout.findViewById(R.id.drawerList);
+        adapter = new NavDrawerRecycler(getActivity(), getData());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return layout;
+    }
+
+    public static List<information> getData(){
+        List<information> data = new ArrayList<>();
+
+        int[] icons = {R.mipmap.ic_account_balance_grey, R.mipmap.ic_explore_grey,  R.mipmap.ic_redeem_grey, R.mipmap.ic_account_circle_grey,};
+        String[] titles = {"Mis Cursos", "Cursos",  "Beneficios", "Perfil",};
+        for (int i = 0; i < titles.length; i++){
+            information currentaData = new information();
+
+            currentaData.iconId = icons[i];
+            currentaData.title = titles[i];
+
+            data.add(currentaData);
+        }
+        return data;
+    }
+
+    public void setUp(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar){
+        containerView = getActivity().findViewById(fragmentId);
+        mDrawerLayout = drawerLayout;
+        mDrawerToogle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                if(!mFromSavedInstanceState){
+                    mFromSavedInstanceState = true;
+                    saveToPreference(getActivity(), KEY_USER_LEARNED_DRAWER, mFromSavedInstanceState+"");
+                }
+
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                if(slideOffset < 0.6){
+                    toolbar.setAlpha(1-slideOffset);
+                }
+            }
+        };
+
+
+        if (!mUserLearnedDrawer && !mFromSavedInstanceState){
+            mDrawerLayout.openDrawer(containerView);
+        }
+
+        mDrawerLayout.setDrawerListener(mDrawerToogle);
+
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToogle.syncState();
+            }
+        });
+    }
+
+
+    public static void saveToPreference(Context context, String preferenceName, String preferenceValue){
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(preferenceName, preferenceValue);
+        editor.apply();
+
+    }
+
+    public static String readToPreference(Context context, String preferenceName, String preferenceValue){
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(preferenceName, preferenceValue);
+    }
+}
